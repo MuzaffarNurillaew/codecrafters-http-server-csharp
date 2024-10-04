688ba8a1761385397e6541585be0e0375b676fc7
Muzaffar Nurillayev <muzaffar_nurillayev@epam.com>
2024-10-04 16:45:48 +0500

    Respond with 200

--- src/Server.cs
+++ src/Server.cs
@@ -5,8 +5,8 @@ using System.Text;
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 Console.WriteLine("Logs from your program will appear here!");
 
-// Uncomment this block to pass the first stage
 var server = new TcpListener(IPAddress.Any, 4221);
+// Starting server
 server.Start();
 var socket = server.AcceptSocket();
 
